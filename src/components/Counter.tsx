de7dc5db5ab9ce758a6ed5b6b1a7d71d05d67e35
caja.prat <caja.prat@cademartori.cl>
2025-07-17 19:06:57 +0000

    🤡 Transitions
    add transitions from astro

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,7 +4,7 @@ export const Counter: React.FC = () => {
 
 	return (
 		<>
-			<button className='rounded-2xl border-white h-10 w-28 text-white bg-blue-950/55 font-bold tracking-widest mt-9 cursor-pointer hover:bg-black/50' onClick={()=>{}}>¡Start!</button>
+			<a className='rounded-2xl border-white h-10 w-28 text-white bg-blue-950/55 font-bold tracking-widest mt-9 cursor-pointer hover:bg-black/50 flex justify-center items-center' href='/quizz'>¡Start!</a>
 		</>
 	)
 }
